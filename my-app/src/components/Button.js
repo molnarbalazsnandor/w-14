56ea2b2c351780b3d7789c19b8c09f845f743483
molnarbalazsnandor <molnarbalazsnandor@gmail.com>
2022-11-29 12:14:18 +0100

    szöveges state

--- my-app/src/components/Button.js
+++ my-app/src/components/Button.js
@@ -25,4 +25,33 @@ function Button() {
   );
 }
 
+/* function Button() {
+  const [buttonText, setButtonText] = useState("click here");
+
+  let number = 1;
+  return (
+    <>
+      <button
+        onClick={() => {
+          number = number + 1;
+          console.log(number);
+        }}
+      >
+        Button {number}
+      </button>
+      <button
+        onClick={() => {
+          if (buttonText === "click here") {
+            setButtonText("clicked");
+          } else {
+            setButtonText("stop clicking");
+          }
+        }}
+      >
+        {buttonText}
+      </button>
+    </>
+  );
+} */
+
 export default Button;
